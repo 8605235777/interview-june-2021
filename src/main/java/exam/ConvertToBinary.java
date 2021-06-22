977e40d632284f260dd7d46ee653324381d192b9
Tajane Sejal Dattatray <86231707+8605235777@users.noreply.github.com>
2021-06-22 12:09:53 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,20 @@
 package exam;
 
-public class ConvertToBinary {
-
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+public class DecimalToBinary
+{
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+    int num = 14, tmp;
+    tmp = num;
+    //int numberSystem = 2;
+    String binary = "";
+    while(num >= 2)
+    {
+      binary = num%2 + binary ;
+      num = num /2;
+    }
+    binary = num + binary ;
+    System.out.println("Binary of " + tmp + " is " + binary);
   }
 }
